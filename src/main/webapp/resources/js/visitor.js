3380792834af58aa0979015712c7fd91cf7dd5eb
Owner <Owner@WINDOWS-G4Q3IKP.localdomain>
2016-11-29 16:14:41 +0900

    마지막 마크업 가져오기

--- src/main/webapp/resources/js/visitor.js
+++ src/main/webapp/resources/js/visitor.js
@@ -1,35 +1,15 @@
-/**
-* 목적 : 참고사항 글자수 체크
-* 매개변수 : 없음
-* 반환값 : 없음
-* 개정이력 : 없음
-*/
-$(function(){
-    $('#write').each(function(){
-        var $maxcount = $('.reserve');
-        var $input = $(".visitWrite");
-        var $count = $(".count");
-        var maximumCount = $maxcount.text() * 1;
-
-        var update = function(){
-            var before = $count.text() * 1;
-            var now = $input.val().length;
-           
-            // 사용자가 입력한 값이 제한 값을 초과하는지를 검사한다.
-            if (now > maximumCount) {
-                var str = $input.val();
-                $("#refCn").focus();
-                var inputVal = str.substr(0, maximumCount);
-                alert($maxcount.text() +'자를 초과하였습니다.');
-                $input.val(inputVal);
+    function ran_col() {
+                var color = '#';
+                var letters = ['F2C2E2','F2D4B6','E8F2B6','B8E5DA','B5B5E5'];
+                color += letters[Math.floor(Math.random() * letters.length)];
+                document.getElementById('yellow').style.background = color;
+                var color = '#';
+                var letters = ['F2C2E2','F2D4B6','E8F2B6','B8E5DA','B5B5E5'];
+                color += letters[Math.floor(Math.random() * letters.length)];
+                document.getElementById('pink').style.background = color;
+                var color = '#';
+                var letters = ['F2C2E2','F2D4B6','E8F2B6','B8E5DA','B5B5E5'];
+                color += letters[Math.floor(Math.random() * letters.length)];
+                document.getElementById('blue').style.background = color;
             }
-           
-            // 필요한 경우 DOM을 수정한다.
-            $count.text(now);
-        };
-        $input.bind('input keyup paste', function(){
-            setTimeout(update, 0);
-        });
-        update();
-    });
-});
+
