e2b74410104db77f887a6ccd0b1eee7530bc0c52
rachel <rachel@192.168.0.10>
2016-12-12 21:39:16 +0900

    에러페이지추가 완료

--- src/main/java/co/greatkorea/www/comm/web/CommonController.java
+++ src/main/java/co/greatkorea/www/comm/web/CommonController.java
@@ -10,12 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CommonController {
 
-	@RequestMapping(value="/error.do", method=RequestMethod.GET)
-	public String errorView(HttpServletResponse res, Model model){
+	@RequestMapping(value="/errorView.do", method=RequestMethod.GET)
+	public String errorView(HttpServletResponse res, Model model) throws Exception{
 		res.setStatus(HttpServletResponse.SC_OK);
-		//logger.warn("404 error occurred");
-		model.addAttribute("contents","common/error");
-		
-		return "/index";
+		return "/common/error";
 	}
 }
